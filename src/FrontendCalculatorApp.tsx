ff8edc1524b7d6c2b255736bbc28f8760486b90c
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-10-06 19:17:13 -0500

    First commit

---
+++ src/FrontendCalculatorApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FrontendCalculatorApp = () => {
+    return (
+        <div>
+            <h1>FrontendCalculatorApp</h1>
+        </div>
+    );
+};
+
+export default FrontendCalculatorApp;
