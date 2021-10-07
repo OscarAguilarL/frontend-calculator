1500a3f47ac1e036d4348dc9591ee82eee30c313
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-10-07 09:45:34 -0500

    Finish design and layout

--- src/FrontendCalculatorApp.tsx
+++ src/FrontendCalculatorApp.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
+
+import './FrontendCalculatorApp.css';
+
 import {ButtonCalc} from "./component/ButtonCalc/ButtonCalc";
 
 const FrontendCalculatorApp = () => {
     return (
-        <div>
-            <ButtonCalc text="+" type="white"/>
-            <ButtonCalc text="+" type="light-blue"/>
-            <ButtonCalc text="+" type="orange"/>
-            <ButtonCalc text="+" type="blue"/>
+        <div className="calculator">
+
+            <div className="calculator-screen">
+                <p className="smallResult">234234</p>
+                <p className="result">123123123</p>
+            </div>
+
+            <div className="buttonRow">
+                <ButtonCalc text="C" type="orange"/>
+                <ButtonCalc text="+/-" type="white"/>
+                <ButtonCalc text="%" type="white"/>
+                <ButtonCalc text="/" type="light-blue"/>
+            </div>
+            <div className="buttonRow">
+                <ButtonCalc text="7" type="white"/>
+                <ButtonCalc text="8" type="white"/>
+                <ButtonCalc text="9" type="white"/>
+                <ButtonCalc text="X" type="light-blue"/>
+            </div>
+            <div className="buttonRow">
+                <ButtonCalc text="4" type="white"/>
+                <ButtonCalc text="5" type="white"/>
+                <ButtonCalc text="6" type="white"/>
+                <ButtonCalc text="-" type="light-blue"/>
+            </div>
+            <div className="buttonRow">
+                <ButtonCalc text="1" type="white"/>
+                <ButtonCalc text="2" type="white"/>
+                <ButtonCalc text="3" type="white"/>
+                <ButtonCalc text="+" type="light-blue"/>
+            </div>
+            <div className="buttonRow">
+                <ButtonCalc text="0" type="white"/>
+                <ButtonCalc text="." type="white"/>
+                <ButtonCalc text="del" type="light-blue"/>
+                <ButtonCalc text="=" type="blue"/>
+            </div>
         </div>
     );
 };
